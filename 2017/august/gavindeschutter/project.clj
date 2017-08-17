65122b191a7e0fd6b388c4a80a4f242574f71ab1
Gavin 'Everyteen' DeSchutter <gavin@nationbuilder.com>
2017-08-16 20:21:31 -0700

    it blends!

--- 2017/august/gavindeschutter/project.clj
+++ 2017/august/gavindeschutter/project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/spec.alpha "0.1.123"]
+                 [csv-map "0.1.2"]
+                 [org.clojure/test.check "0.9.0"]])
--- 2017/august/gavindeschutter/src/dsotmc_august_2017/core.clj
+++ 2017/august/gavindeschutter/src/dsotmc_august_2017/core.clj
@@ -1,6 +1,36 @@
-(ns dsotmc-august-2017.core)
+(ns dsotmc-august-2017.core
+  (:require [csv-map.core :refer [parse-csv]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def parking-map
+  {:package-type "pkgtype"
+   :prefix "prefix"
+   :timeframe "pkgenday"
+   :suffix "suffix"
+   :lat "Y"
+   :lng "﻿X"
+   :aorp "aorp"
+   :evtarea "evtarea"
+   :address "address"
+   :begin-at "enbegin"
+   :permit-area "permitarea"
+   :parkmob "parkmob"
+   :end-at "enend"
+   :id "objectid"
+   :object-code "obj_code"
+   :max-rate "maxrate"
+   :gis-object-id "gisobjid"
+   :street "street"
+   :timelimit "timelimit"})
+
+(defn- build-object [row map]
+  (reduce (fn [a [k v]] (assoc a k (get row v)))
+          {}
+          map))
+
+
+(defn build-parking [row]
+  (build-object row parking-map))
+
+
+(def objs (map build-parking
+               (-> (slurp "ONSTREETPARKING.csv") parse-csv)))
